a4234470530372c55891f4b838684ca59edc74f4
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2021-10-16 18:27:26 -0300

    last deploy

--- src/main/java/com/ufitness/ufitness/controller/mail/MailController.java
+++ src/main/java/com/ufitness/ufitness/controller/mail/MailController.java
@@ -3,17 +3,15 @@ package com.ufitness.ufitness.controller.mail;
 import com.ufitness.ufitness.service.mail.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/ufitness/mail")
+@RequestMapping("")
 public class MailController {
     private final MailService mailService;
 
@@ -22,7 +20,7 @@ public class MailController {
         this.mailService = mailService;
     }
 
-    @PatchMapping("/confirm")
+    @RequestMapping(value = "/api/v1/ufitness/mail/confirm", produces = MediaType.APPLICATION_JSON_VALUE, method = {RequestMethod.GET, RequestMethod.PATCH})
     public ResponseEntity<Map<String, String>> validateMailToken(@RequestParam("token") String token) {
         mailService.validateMailToken(token);
         return ResponseEntity.status(HttpStatus.OK).body(Collections.singletonMap("action", "SUCCESS"));
